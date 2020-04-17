fb02b54e79c745f128ef5e670a9de3f451aafd58
Fedir Pushkov <f.pushkov@g.nsu.ru>
2020-04-17 19:44:28 +0700

    Add comment in dragInProgress reducer

--- src/reducers/dragInProgress.js
+++ src/reducers/dragInProgress.js
@@ -1,4 +1,8 @@
-export default (state, { value, draggedThoughtsRanked }) => ({
-  dragInProgress: value,
-  draggedThoughtsRanked: state.draggedThoughtsRanked ? (!draggedThoughtsRanked ? draggedThoughtsRanked : state.draggedThoughtsRanked) : draggedThoughtsRanked
-})
+export default (state, { value, draggedThoughtsRanked }) => {
+  console.log(state.draggedThoughtsRanked)
+  return {
+    dragInProgress: value,
+    // Prevent setting new draggedThoughtRanked before, if previous value wasn't reset to undefined
+    draggedThoughtsRanked: state.draggedThoughtsRanked ? (!draggedThoughtsRanked ? undefined : state.draggedThoughtsRanked) : draggedThoughtsRanked
+  }
+}
