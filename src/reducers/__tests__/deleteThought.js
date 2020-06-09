c8fcccf074e97dd1b4fadb4884c3e1979f44d989
Raine Revere <raine@cybersemics.org>
2020-06-09 13:27:08 -0600

    Test: deleteThought refactor.

--- src/reducers/__tests__/deleteThought.js
+++ src/reducers/__tests__/deleteThought.js
@@ -3,23 +3,19 @@ import { initialState, reducerFlow } from '../../util'
 import { exportContext } from '../../selectors'
 
 // reducers
-import newThought from '../newThought'
-import deleteThought from '../deleteThought'
-import setCursor from '../setCursor'
-import cursorBack from '../cursorBack'
-import cursorUp from '../cursorUp'
+import {
+  cursorBack,
+  cursorUp,
+  deleteThought,
+  newThought,
+  setCursor,
+} from '../../reducers'
 
 it('delete thought within root', () => {
 
   const steps = [
-
-    // new thought 1 in root
     state => newThought(state, { value: 'a' }),
-
-    // new thought 2 in root
     state => newThought(state, { value: 'b' }),
-
-    // delete thought
     deleteThought
   ]
 
@@ -35,17 +31,9 @@ it('delete thought within root', () => {
 it('delete thought with no cursor should do nothing ', () => {
 
   const steps = [
-
-    // new thought 1 in root
     state => newThought(state, { value: 'a' }),
-
-    // new thought 2 in root
     state => newThought(state, { value: 'b' }),
-
-    // clear cursor
     state => setCursor(state, { thoughtsRanked: null }),
-
-    // delete thought
     deleteThought
   ]
 
@@ -62,14 +50,8 @@ it('delete thought with no cursor should do nothing ', () => {
 it('delete thought within context', () => {
 
   const steps = [
-
-    // new thought 1 in root
     state => newThought(state, { value: 'a' }),
-
-    // new subthought
     state => newThought(state, { value: 'a1', insertNewSubthought: true }),
-
-    // delete thought
     deleteThought
   ]
 
@@ -85,19 +67,10 @@ it('delete thought within context', () => {
 it('delete descendants', () => {
 
   const steps = [
-
-    // new thought 1 in root
     state => newThought(state, { value: 'a' }),
-
-    // new subthought
     state => newThought(state, { value: 'a1', insertNewSubthought: true }),
-
-    // new subthought
     state => newThought(state, { value: 'a1.1', insertNewSubthought: true }),
-
     cursorBack,
-
-    // delete thought
     deleteThought
   ]
 
@@ -113,22 +86,11 @@ it('delete descendants', () => {
 it('cursor should move to prev sibling', () => {
 
   const steps = [
-
-    // new thought in root
     state => newThought(state, { value: 'a' }),
-
-    // new subthought 1
     state => newThought(state, { value: 'a1', insertNewSubthought: true }),
-
-    // new subthought 2
     state => newThought(state, { value: 'a2' }),
-
-    // new subthought 3
     state => newThought(state, { value: 'a3' }),
-
     cursorUp,
-
-    // delete thought
     deleteThought,
   ]
 
@@ -143,23 +105,12 @@ it('cursor should move to prev sibling', () => {
 it('cursor should move to next sibling if there is no prev sibling', () => {
 
   const steps = [
-
-    // new thought in root
     state => newThought(state, { value: 'a' }),
-
-    // new subthought 1
     state => newThought(state, { value: 'a1', insertNewSubthought: true }),
-
-    // new subthought 2
     state => newThought(state, { value: 'a2' }),
-
-    // new subthought 3
     state => newThought(state, { value: 'a3' }),
-
     cursorUp,
     cursorUp,
-
-    // delete thought
     deleteThought,
   ]
 
@@ -174,14 +125,8 @@ it('cursor should move to next sibling if there is no prev sibling', () => {
 it('cursor should move to parent if the deleted thought has no siblings', () => {
 
   const steps = [
-
-    // new thought in root
     state => newThought(state, { value: 'a' }),
-
-    // new subthought 1
     state => newThought(state, { value: 'a1', insertNewSubthought: true }),
-
-    // delete thought
     deleteThought,
   ]
 
@@ -196,11 +141,7 @@ it('cursor should move to parent if the deleted thought has no siblings', () => 
 it('cursor should be removed if the last thought is deleted', () => {
 
   const steps = [
-
-    // new thought in root
     state => newThought(state, { value: 'a' }),
-
-    // delete thought
     deleteThought,
   ]
 
