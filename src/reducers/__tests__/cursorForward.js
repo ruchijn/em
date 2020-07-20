688bfa769f9e1d250e5fbb3ba4e378a94a801ba1
Raine Revere <raine@cybersemics.org>
2020-07-20 17:41:34 -0600

    cursorForward: Add context view tests.

--- src/reducers/__tests__/cursorForward.js
+++ src/reducers/__tests__/cursorForward.js
@@ -1,10 +1,13 @@
 import { initialState, reducerFlow } from '../../util'
-
-// reducers
-import cursorBack from '../cursorBack'
-import cursorForward from '../cursorForward'
-import newThought from '../newThought'
-import setCursor from '../setCursor'
+import {
+  cursorBack,
+  cursorDown,
+  cursorForward,
+  cursorUp,
+  newThought,
+  setCursor,
+  toggleContextView,
+} from '../../reducers'
 
 it('reverse cursorBack', () => {
 
@@ -51,5 +54,68 @@ it('move to first child if there is no cursor', () => {
 
   expect(stateNew.cursor)
     .toMatchObject([{ value: 'a', rank: 0 }])
+
+})
+
+describe('context view', () => {
+
+  // create a minimal structure for context view
+  const startSteps = [
+    newThought({ value: 'a' }),
+    newThought({ value: 'm', insertNewSubthought: true }),
+    newThought({ value: 'x', insertNewSubthought: true }),
+    cursorUp,
+    cursorUp,
+    newThought({ value: 'b' }),
+    newThought({ value: 'm', insertNewSubthought: true }),
+  ]
+
+  it('reverse cursorBack', () => {
+
+    const steps = [
+      ...startSteps,
+      toggleContextView,
+      cursorDown,
+      cursorBack,
+      cursorForward,
+    ]
+
+    const stateNew = reducerFlow(steps)(initialState())
+
+    expect(stateNew.cursor)
+      .toMatchObject([{ value: 'b', rank: 1 }, { value: 'm', rank: 0 }, { value: 'a', rank: 0 }])
+
+  })
+
+  it('move to first context if there is no history', () => {
+
+    const steps = [
+      ...startSteps,
+      toggleContextView,
+      cursorForward,
+    ]
+
+    const stateNew = reducerFlow(steps)(initialState())
+
+    expect(stateNew.cursor)
+      .toMatchObject([{ value: 'b', rank: 1 }, { value: 'm', rank: 0 }, { value: 'a', rank: 0 }])
+
+  })
+
+  it('move to first child of context if there is no history', () => {
+
+    const steps = [
+      ...startSteps,
+      toggleContextView,
+      cursorForward,
+      cursorForward,
+    ]
+
+    const stateNew = reducerFlow(steps)(initialState())
+
+    expect(stateNew.cursor)
+      .toMatchObject([{ value: 'b', rank: 1 }, { value: 'm', rank: 0 }, { value: 'a', rank: 0 }, { value: 'x', rank: 0 }])
+
+  })
 
 })
